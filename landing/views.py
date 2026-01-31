bf8e974ce8331f7cb149b1264933d0e477859452
Pedro Morese <pmorese1505@gmail.com>
2026-01-31 16:14:59 -0400

    initial commit

---
+++ landing/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+
+def index(request):
+    return render(request, 'landing/index.html')
+
+def login_view(request):
+    return render(request, 'landing/login.html')
+
+def register_view(request):
+    return render(request, 'landing/register.html')
+
+def health_check(request):
+    return JsonResponse({
+        'status': 'OK',
+        'message': 'Servidor Django corriendo correctamente',
+        'app': 'EduPlatform'
+    })
